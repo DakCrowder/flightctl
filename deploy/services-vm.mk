441875a775b849c7085b5486c64b5d4d36e9a639
Dakota Crowder <dcrowder@redhat.com>
2025-05-08 17:32:28 -0400

    EDM-1520: WIP

--- deploy/services-vm.mk
+++ deploy/services-vm.mk
@@ -1,12 +1,28 @@
 
 services-container: rpm
-	podman build -t flightctl-services-image -f test/scripts/services-images/Containerfile.services .
+	sudo podman build -t flightctl-services:latest -f test/scripts/services-images/Containerfile.services .
 
 run-services-container:
-	podman run -d --network host --name flightctl-services localhost/flightctl-services-image
+	sudo podman run -d --privileged --replace \
+	--name flightctl-services \
+	-p 8080:443 \
+	-p 3443:3443 \
+	-p 8090:8090 \
+	localhost/flightctl-services:latest
 
 clean-services-container:
-	podman stop flightctl-services-container || true
-	podman rm flightctl-services-container || true
+	sudo podman stop flightctl-services || true
+	sudo podman rm flightctl-services || true
+
+build-services-qcow:
+	mkdir -p bin/output && \
+	sudo podman run --rm -it --privileged --pull=newer \
+		--security-opt label=type:unconfined_t \
+		-v "${PWD}/bin/output":/output \
+		-v /var/lib/containers/storage:/var/lib/containers/storage \
+		quay.io/centos-bootc/bootc-image-builder:latest \
+		--type qcow2 \
+		--local \
+		localhost/flightctl-services:latest
 
 .PHONY: services-container run-services-container clean-services-container
